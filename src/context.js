17db10cf16c0261113cdc637274688a5ca6d3ad7
Sohum Banerjea <sohumb@gmail.com>
2015-07-08 18:06:53 +1000

    s/asset/stock

--- src/context.js
+++ src/context.js
@@ -30,8 +30,8 @@ export default Morearty.createContext({
       id: 4,
       name: 'Deborah'
     }],
-    // we're using "assets" to mean the classes of shares
-    assets: [{
+    // we're using "stocks" to mean the classes of shares
+    stocks: [{
       id: 1,
       name: 'Jordan is Resistance', // JR
       opposite: 2
@@ -64,22 +64,22 @@ export default Morearty.createContext({
       { type: 'trade', party: 4, counterparty: null, receiving: startingWorth, sending: null, date: start },
 
       // Aberforth thinks Jordan is a spy. He tests the waters by trying to sell some of his JRs
-      { type: 'order', party: 1, offering: { asset: 1 }, asking: { money: 60 }, quantity: 100, date: step1 },
+      { type: 'order', party: 1, offering: { stock: 1 }, asking: { money: 60 }, quantity: 100, date: step1 },
 
       // Cassie thinks Jordan is resistance. She sees this sell order and decides to snap up a bunch,
       // and she's happy to drive the price for JRs up, too
-      { type: 'order', party: 3, offering: { money: 75 }, asking: { asset: 1 }, quantity: 500, date: step2 },
+      { type: 'order', party: 3, offering: { money: 75 }, asking: { stock: 1 }, quantity: 500, date: step2 },
 
       // The system sees these matching buy and sell orders, and executes the trade
       // principle: trade the minimum assets/balance necessary to fulfil both sides
-      { type: 'trade', party: 1, counterparty: 3, receiving: [{ money: 6000 }], sending: [{ asset: 1, quantity: 100 }], date: step2 },
+      { type: 'trade', party: 1, counterparty: 3, receiving: [{ money: 6000 }], sending: [{ stock: 1, quantity: 100 }], date: step2 },
 
       // so the computed state of the market now is that Cassie has an outstanding buy order
       // for 400 JRs, for upto $75 each
 
       // Now Deborah decides she believes Jordan is a spy, but she isn't paying attention to the market
       // She puts in a buy order for cheap JSs
-      { type: 'order', party: 4, offering: { money: 40 }, asking: { asset: 2 }, quantity: 200, date: step3 },
+      { type: 'order', party: 4, offering: { money: 40 }, asking: { stock: 2 }, quantity: 200, date: step3 },
 
       // if she'd been paying attention, she would have seen that she had an arbitrage opportunity.
       // The market is always willing to trade $100 for a matched pair, in any direction
@@ -90,8 +90,8 @@ export default Morearty.createContext({
       // out of the economy, but the reverse auto-arbitrage effectively puts it back, so we don't
       // think this is a major issue
       { type: 'arbitrage', trades: [
-        { type: 'trade', party: 3, counterparty: null, receiving: [{ asset: 1, quantity: 200 }], sending: [{ money: 15000 }]},
-        { type: 'trade', party: 4, counterparty: null, receiving: [{ asset: 2, quantity: 200 }], sending: [{ money: 8000 }]},
+        { type: 'trade', party: 3, counterparty: null, receiving: [{ stock: 1, quantity: 200 }], sending: [{ money: 15000 }]},
+        { type: 'trade', party: 4, counterparty: null, receiving: [{ stock: 2, quantity: 200 }], sending: [{ money: 8000 }]},
       ], date: step3 }
 
     ]
