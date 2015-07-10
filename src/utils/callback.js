d41ebd223151883e9a511bde0efad84a4e0a7e96
Sohum Banerjea <sohumb@gmail.com>
2015-07-10 22:15:31 +1000

    Re-exports of Morearty.{DOM, Callback}

---
+++ src/utils/callback.js
@@ -0,0 +1,4 @@
+/* es6-friendly re-export of Morearty.Callback */
+
+import Morearty from 'morearty';
+export default Morearty.Callback;
---
+++ src/utils/dom.js
@@ -0,0 +1,4 @@
+/* es6-friendly re-export of Morearty.DOM */
+
+import Morearty from 'morearty';
+export default Morearty.DOM;
