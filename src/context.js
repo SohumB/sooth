1c6f09459f39baa7fb1340fba91a1133902d2230
Sohum Banerjea <sohumb@gmail.com>
2015-07-10 22:14:38 +1000

    Switch context collections to maps indexed by id

--- src/context.js
+++ src/context.js
@@ -17,70 +17,64 @@ const step3 = new Date('2015-07-07 11:00:00Z');
 
 export default Morearty.createContext({
   initialState: {
-    users: [{
-      id: 1,
-      name: 'Aberforth'
-    }, {
-      id: 2,
-      name: 'Bertrand'
-    }, {
-      id: 3,
-      name: 'Cassie'
-    }, {
-      id: 4,
-      name: 'Deborah'
-    }],
+    users: {
+      aberforth: { name: 'Aberforth' },
+      bertrand: { name: 'Bertrand '},
+      cassie: { name: 'Cassie' },
+      deb: { name: 'Deborah' }
+    },
     // we're using "stocks" to mean the classes of shares
-    stocks: [{
-      id: 1,
-      name: 'Jordan is Resistance', // JR
-      opposite: 2
-    }, {
-      id: 2,
-      name: 'Jordan is a Spy', // JS
-      opposite: 1
-    }, {
-      id: 3,
-      name: 'Marigold is Resistance', // MR
-      opposite: 4
-    }, {
-      id: 4,
-      name: 'Marigold is a Spy', // MS
-      opposite: 3
-    }, {
-      id: 5,
-      name: 'Nils is Resistance', // NR
-      opposite: 6
-    }, {
-      id: 6,
-      name: 'Nils is a Spy', // NS
-      opposite: 5
-    }],
+    stocks: {
+      JR: {
+        name: 'Jordan is Resistance',
+        opposite: 'JS'
+      },
+      JS: {
+        name: 'Jordan is a Spy',
+        opposite: 'JR'
+      },
+      MR: {
+        name: 'Marigold is Resistance',
+        opposite: 'MS'
+      },
+      MS: {
+        name: 'Marigold is a Spy',
+        opposite: 'MR'
+      },
+      NR: {
+        name: 'Nils is Resistance',
+        opposite: 'NS'
+      },
+      NS: {
+        name: 'Nils is a Spy',
+        opposite: 'NR'
+      }
+    },
     market: [
       // these trades with counterparty: null and sending: null are used to set up the game
-      { type: 'trade', party: 1, counterparty: null, receiving: startingWorth, sending: null, date: start },
-      { type: 'trade', party: 2, counterparty: null, receiving: startingWorth, sending: null, date: start },
-      { type: 'trade', party: 3, counterparty: null, receiving: startingWorth, sending: null, date: start },
-      { type: 'trade', party: 4, counterparty: null, receiving: startingWorth, sending: null, date: start },
+      { type: 'trade', party: 'aberforth', counterparty: null, receiving: startingWorth, sending: null, date: start },
+      { type: 'trade', party: 'bertrand', counterparty: null, receiving: startingWorth, sending: null, date: start },
+      { type: 'trade', party: 'cassie', counterparty: null, receiving: startingWorth, sending: null, date: start },
+      { type: 'trade', party: 'deb', counterparty: null, receiving: startingWorth, sending: null, date: start },
 
       // Aberforth thinks Jordan is a spy. He tests the waters by trying to sell some of his JRs
-      { type: 'order', party: 1, offering: { stock: 1 }, asking: { money: 60 }, quantity: 100, date: step1 },
+      { type: 'order', party: 'aberforth', offering: { stock: 'JR' }, asking: { money: 60 }, quantity: 100, date: step1 },
 
       // Cassie thinks Jordan is resistance. She sees this sell order and decides to snap up a bunch,
       // and she's happy to drive the price for JRs up, too
-      { type: 'order', party: 3, offering: { money: 75 }, asking: { stock: 1 }, quantity: 500, date: step2 },
+      { type: 'order', party: 'cassie', offering: { money: 75 }, asking: { stock: 'JR' }, quantity: 500, date: step2 },
 
       // The system sees these matching buy and sell orders, and executes the trade
       // principle: second mover advantage, so you can put in an order that takes advantage of an
       // existing counterparty order, but then settle down to your actual price
-      { type: 'trade', party: 1, counterparty: 3, receiving: [{ money: 6000 }], sending: [{ stock: 1, quantity: 100 }], date: step2 },
+      { type: 'trade', party: 'aberforth', counterparty: 'cassie', receiving: [{ money: 6000 }], sending: [{ stock: 'JR', quantity: 100 }], date: step2 },
 
       // so the computed state of the market now is that Cassie has an outstanding buy order
       // for 400 JRs, for upto $75 each
 
       // Now Deborah decides she believes Jordan is a spy, but she isn't paying attention to the market
       // She puts in a buy order for cheap JSs
-      { type: 'order', party: 4, offering: { money: 40 }, asking: { stock: 2 }, quantity: 200, date: step3 },
+      { type: 'order', party: 'deb', offering: { money: 40 }, asking: { stock: 'JS' }, quantity: 200, date: step3 },
 
       // if she'd been paying attention, she would have seen that she had an arbitrage opportunity.
       // The market is always willing to trade $100 for a matched pair, in any direction
@@ -91,8 +85,8 @@ export default Morearty.createContext({
       // out of the economy, but the reverse auto-arbitrage effectively puts it back, so we don't
       // think this is a major issue
       { type: 'arbitrage', trades: [
-        { type: 'trade', party: 3, counterparty: null, receiving: [{ stock: 1, quantity: 200 }], sending: [{ money: 15000 }]},
-        { type: 'trade', party: 4, counterparty: null, receiving: [{ stock: 2, quantity: 200 }], sending: [{ money: 8000 }]},
+        { type: 'trade', party: 'cassie', counterparty: null, receiving: [{ stock: 'JR', quantity: 200 }], sending: [{ money: 15000 }]},
+        { type: 'trade', party: 'deb', counterparty: null, receiving: [{ stock: 'JS', quantity: 200 }], sending: [{ money: 8000 }]},
       ], date: step3 }
 
     ]
