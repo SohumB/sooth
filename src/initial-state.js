18b69bcfffae2e709f04410342be59b870ce2bd8
Sohum Banerjea <sohumb@gmail.com>
2015-07-11 20:35:02 +1000

    Switch to implicit underwriting/negative stock quantity allowed

--- src/initial-state.js
+++ src/initial-state.js
@@ -1,11 +1,8 @@
 const startingWorth = [
   { money: 500000 },
-  { stock: 'JR', quantity: 1000 },
-  { stock: 'JS', quantity: 1000 },
-  { stock: 'MR', quantity: 1000 },
-  { stock: 'MS', quantity: 1000 },
-  { stock: 'NR', quantity: 1000 },
-  { stock: 'NS', quantity: 1000 }
+  { stock: 'JS', quantity: 100 },
+  { stock: 'MS', quantity: 100 },
+  { stock: 'NS', quantity: 100 }
 ];
 
 const start = new Date('2015-07-07 08:00:00Z');
@@ -22,30 +19,9 @@ export default {
   },
   // we're using "stocks" to mean the classes of shares
   stocks: {
-    JR: {
-      name: 'Jordan is Resistance',
-      opposite: 'JS'
-    },
-    JS: {
-      name: 'Jordan is a Spy',
-      opposite: 'JR'
-    },
-    MR: {
-      name: 'Marigold is Resistance',
-      opposite: 'MS'
-    },
-    MS: {
-      name: 'Marigold is a Spy',
-      opposite: 'MR'
-    },
-    NR: {
-      name: 'Nils is Resistance',
-      opposite: 'NS'
-    },
-    NS: {
-      name: 'Nils is a Spy',
-      opposite: 'NR'
-    }
+    JS: { name: 'Jordan is a Spy' },
+    MS: { name: 'Marigold is a Spy' },
+    NS: { name: 'Nils is a Spy' }
   },
   market: [
     // these trades with counterparty: null and sending: null are used to set up the game
@@ -54,38 +30,24 @@ export default {
     { type: 'trade', party: 'cassie', counterparty: null, receiving: startingWorth, sending: null, date: start },
     { type: 'trade', party: 'deb', counterparty: null, receiving: startingWorth, sending: null, date: start },
 
-    // Aberforth thinks Jordan is a spy. He tests the waters by trying to sell some of his JRs
-    { type: 'order', party: 'aberforth', offering: { stock: 'JR' }, asking: { money: 60 }, quantity: 100, date: step1 },
+    // Aberforth thinks Jordan is a spy. He tests the waters by trying to buy some JSs
+    { type: 'order', party: 'aberforth', offering: { money: 40 }, asking: { stock: 'JS' }, quantity: 200, date: step1 },
 
-    // Cassie thinks Jordan is resistance. She sees this sell order and decides to snap up a bunch,
-    // and she's happy to drive the price for JRs up, too
-    { type: 'order', party: 'cassie', offering: { money: 75 }, asking: { stock: 'JR' }, quantity: 500, date: step2 },
+    // Cassie thinks Jordan is resistance. She sees this buy order and decides to take advantage
+    // and she's happy to drive the price for JSs down, too
+    { type: 'order', party: 'cassie', offering: { stock: 'JS' }, asking: { money: 25 }, quantity: 500, date: step2 },
 
     // The system sees these matching buy and sell orders, and executes the trade
     // principle: second mover advantage, so you can put in an order that takes advantage of an
     // existing counterparty order, but then settle down to your actual price
-    { type: 'trade', party: 'aberforth', counterparty: 'cassie', receiving: [{ money: 6000 }], sending: [{ stock: 'JR', quantity: 100 }], date: step2 },
+    { type: 'trade', party: 'aberforth', counterparty: 'cassie', receiving: [{ stock: 'JS', quantity: 200 }], sending: [{ money: 8000 }], date: step2 },
 
-    // so the computed state of the market now is that Cassie has an outstanding buy order
-    // for 400 JRs, for upto $75 each
+    // note that Cassie now has -100 JSs. This is completely fine, and no one cares.
+    // If she still has any outstanding -JSs by the time the market closes, the market
+    // will force her to buy them at the then known true value.
 
-    // Now Deborah decides she believes Jordan is a spy, but she isn't paying attention to the market
-    // She puts in a buy order for cheap JSs
-    { type: 'order', party: 'deb', offering: { money: 40 }, asking: { stock: 'JS' }, quantity: 200, date: step3 },
-
-    // if she'd been paying attention, she would have seen that she had an arbitrage opportunity.
-    // The market is always willing to trade $100 for a matched pair, in any direction
-    // so she should have bought 200 JR/JS pairs from the market, and sold the JR halves to Cassie
-    // effectively buying the the JS halves for $25, not $40.
-
-    // As is, the market is perfectly happy to do that auto-arbitrage for her. This drains money
-    // out of the economy, but the reverse auto-arbitrage effectively puts it back, so we don't
-    // think this is a major issue
-    { type: 'arbitrage', trades: [
-      { type: 'trade', party: 'cassie', counterparty: null, receiving: [{ stock: 'JR', quantity: 200 }], sending: [{ money: 15000 }]},
-      { type: 'trade', party: 'deb', counterparty: null, receiving: [{ stock: 'JS', quantity: 200 }], sending: [{ money: 8000 }]},
-    ], date: step3 }
-
+    // so the computed state of the market now is that Cassie has an outstanding sell order
+    // for 300 JSs, for at least $25 each
   ]
 
 };
